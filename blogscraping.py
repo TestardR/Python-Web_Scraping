8c9c709bb7a2b7d80646151246be96359765ad99
unknown <rmtestard@gmail.com>
2019-04-21 11:47:30 +0200

    building a csv with data

--- blogscraping.py
+++ blogscraping.py
@@ -7,5 +7,14 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 posts = soup.find_all(class_='item')
 
-for post in posts:
-    print(post)
+with open('posts.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['Title', 'Link', 'Date']
+    csv_writer.writerow(headers)
+
+    for post in posts:
+        title = post.find(id="description_title").get_text()
+        technologies = post.find(
+            id="description_title").find_next_sibling().get_text().replace('\n', '').replace('             ', '').replace('            ', ' ')
+        link = post.select('a')[1]['href']
+        csv_writer.writerow([title, technologies, link])
